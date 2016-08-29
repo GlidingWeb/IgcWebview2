853793779a95d1b7acf10e550c767a6fe0389e81
Alistair Green <alistairmgreen@gmail.com>
2016-08-29 23:36:42 +0100

    Code clean-up.
    - Wrap modules in immediately invoked function expressions to avoid creating unneccessary global variables.
    - Reformat code.
    - Fix errors found by jshint (missing semicolons, missing break in switch-case, function created inside a loop).

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 // This is the entry point for the build.  Contains basic user interaction code.
 
 (function () {
+  var $ = require('jquery');
   var apiKeys = require('./apikeys');
 
-  window.ginit=function(){                            //Callback after maps api loads.  Must be in global scope
-  var map=require('./mapctrl');
-  map.initmap();
-  }
+  window.ginit = function () {                            //Callback after maps api loads.  Must be in global scope
+    var map = require('./mapctrl');
+    map.initmap();
+  };
 
-  window.importTask=function(points) {
-  var present=require('./presentation');
-  console.log("imported");
-  present.showImported(points);
-  return "Task Entered";
-  }
+  window.importTask = function (points) {
+    var present = require('./presentation');
+    console.log("imported");
+    present.showImported(points);
+    return "Task Entered";
+  };
 
   var script = document.createElement('script');
   script.type = 'text/javascript';
@@ -21,131 +22,131 @@
   document.body.appendChild(script);
 
   $(document).ready(function () {
-  var preference=require('./preferences');
-  var present=require('./presentation');
-  preference.getStoredValues();
-  present.showPreferences();
+    var preference = require('./preferences');
+    var present = require('./presentation');
+    preference.getStoredValues();
+    present.showPreferences();
 
-  $("#igc").prop("checked", true);  //Firefox ignores markup on refresh
+    $("#igc").prop("checked", true);  //Firefox ignores markup on refresh
 
-  $('#fileControl').change(function() {
+    $('#fileControl').change(function () {
 
-  if (this.files.length > 0) {
-  var reader = new FileReader();
-  reader.onload = function (e) {
-  //  try {                                                                //exception handling temporarily disabled till debugged
-  var igcFile=require('./igc');
-  igcFile.initialise(this.result);
-  present.displayIgc();
-  /*
-  } catch (ex) {
-  if (ex instanceof IGCException) {
-  alert(ex.message);
-  } else {
-  throw ex;
-  }
-  }
-  */
-  };
-  reader.readAsText(this.files[0]);
-  }
-  });
+      if (this.files.length > 0) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          //  try {                                                                //exception handling temporarily disabled till debugged
+          var igcFile = require('./igc');
+          igcFile.initialise(this.result);
+          present.displayIgc();
+          /*
+          } catch (ex) {
+          if (ex instanceof IGCException) {
+          alert(ex.message);
+          } else {
+          throw ex;
+          }
+          }
+          */
+        };
+        reader.readAsText(this.files[0]);
+      }
+    });
 
-  $('#timeSlider').on('input', function () {
-  var t = parseInt($(this).val(), 10);
-  present.showPosition(t);
-  });
+    $('#timeSlider').on('input', function () {
+      var t = parseInt($(this).val(), 10);
+      present.showPosition(t);
+    });
 
-  $('#timeSlider').on('change', function () {
-  var t = parseInt($(this).val(), 10);
-  present.showPosition(t);
-  });
+    $('#timeSlider').on('change', function () {
+      var t = parseInt($(this).val(), 10);
+      present.showPosition(t);
+    });
 
-  $('#zoomtrack').click(function () {
-  present.zoomTrack();
-  });
+    $('#zoomtrack').click(function () {
+      present.zoomTrack();
+    });
 
-  $('#help').click(function () {
-  present.showPreferences();
-  });
+    $('#help').click(function () {
+      present.showPreferences();
+    });
 
-  $('#airclip').change(function() {
-  preference.setAirclip($(this).val());
-  present.airClipChange();
-  });
+    $('#airclip').change(function () {
+      preference.setAirclip($(this).val());
+      present.airClipChange();
+    });
 
-  $('#altitudeunits').change(function() {
-  preference.setAltUnits($(this).val());
-  present.altChange(parseInt($('#timeSlider').val(),10));
-  });
+    $('#altitudeunits').change(function () {
+      preference.setAltUnits($(this).val());
+      present.altChange(parseInt($('#timeSlider').val(), 10));
+    });
 
-  $('#climbunits').change(function() {
-  preference.setClimbUnits($(this).val());
-  });
+    $('#climbunits').change(function () {
+      preference.setClimbUnits($(this).val());
+    });
 
-  $('#lengthunits').change(function() {
-  preference.setLengthUnits($(this).val());
-  present.lengthChange();
-  });
+    $('#lengthunits').change(function () {
+      preference.setLengthUnits($(this).val());
+      present.lengthChange();
+    });
 
-  $('#cruiseunits').change(function() {
-  preference.setCruiseUnits($(this).val());
-  });
+    $('#cruiseunits').change(function () {
+      preference.setCruiseUnits($(this).val());
+    });
 
-  $('#taskunits').change(function() {
-  preference.setTaskUnits($(this).val());
-  });
+    $('#taskunits').change(function () {
+      preference.setTaskUnits($(this).val());
+    });
 
-  $('#unitconfig').click(function() {
-  $('#setunits').show();
-  });
+    $('#unitconfig').click(function () {
+      $('#setunits').show();
+    });
 
-  $('.closewindow').click(function () {
-  $(this).parent().hide();
-  });
+    $('.closewindow').click(function () {
+      $(this).parent().hide();
+    });
 
-  $('#enterTask').click(function(){
-  present.getUserTask();
-  });
+    $('#enterTask').click(function () {
+      present.getUserTask();
+    });
 
-  $('input[type=radio][name=tasksource]').change(function () {
-  preference.setTaskSource(this.id);
-  present.replaceTask(this.id);
-  });
+    $('input[type=radio][name=tasksource]').change(function () {
+      preference.setTaskSource(this.id);
+      present.replaceTask(this.id);
+    });
 
-  $('#sectorconfig').click(function () {
-  $('#sectordefs').show();
-  });
+    $('#sectorconfig').click(function () {
+      $('#sectordefs').show();
+    });
 
-  $('#setsectors').click(function () {
-  present.setSectors();
-  });
+    $('#setsectors').click(function () {
+      present.setSectors();
+    });
 
-  $('#cancelsectors').click(function () {
-  $(this).parent().hide();
-  });
+    $('#cancelsectors').click(function () {
+      $(this).parent().hide();
+    });
 
-  $('#tpdefaults').click(function(){
-  preference.setSectorDefaults();
-  present.showSectorPreferences();
-  });
+    $('#tpdefaults').click(function () {
+      preference.setSectorDefaults();
+      present.showSectorPreferences();
+    });
 
-  $('#altref').click(function () {
-  $('#setaltref').show();
-  });
+    $('#altref').click(function () {
+      $('#setaltref').show();
+    });
 
-  $('#althelp').click(function () {
-  window.open("igchelp.html#alt", "_blank");
-  });
+    $('#althelp').click(function () {
+      window.open("igchelp.html#alt", "_blank");
+    });
 
-  $('#restorealtref').click(function () {
-  $(this).parent().hide();
-  });
+    $('#restorealtref').click(function () {
+      $(this).parent().hide();
+    });
 
-  $('#applyaltref').click(function () {
-  preference.setAltPrefs($("input[name='alttype']").filter(':checked').val(),$("input[name='altsource']").filter(':checked').val());
-  present.altChange(parseInt($('#timeSlider').val(),10));
-  $(this).parent().hide();
-  });
+    $('#applyaltref').click(function () {
+      preference.setAltPrefs($("input[name='alttype']").filter(':checked').val(), $("input[name='altsource']").filter(':checked').val());
+      present.altChange(parseInt($('#timeSlider').val(), 10));
+      $(this).parent().hide();
+    });
   });
 })();
--- src/presentation.js
+++ src/presentation.js
@@ -1,265 +1,268 @@
-//This module handles all presentation tasks that are in simple html- ie: excluding the map and graph
+(function () {
+    //This module handles all presentation tasks that are in simple html- ie: excluding the map and graph
 
-var flight=require('./igc');
-var prefs=require('./preferences');
-var utils=require('./utilities');
-var mapControl=require('./mapctrl.js');
-var barogram=require('./plotgraph');
- var task=require('./task.js');
- var planWindow;
+    var flight = require('./igc');
+    var prefs = require('./preferences');
+    var utils = require('./utilities');
+    var mapControl = require('./mapctrl.js');
+    var barogram = require('./plotgraph');
+    var task = require('./task.js');
+    var planWindow;
 
- function zapTask() {
-     $('#taskentry').hide();
-      $('#task').hide();
-      task.clearTask();
-      mapControl.zapTask();
- }
- 
- function showAltPreferences() {
-if(prefs.altPrefs.altsource==='P') {
-       $("#P").prop("checked", true);
+    function zapTask() {
+        $('#taskentry').hide();
+        $('#task').hide();
+        task.clearTask();
+        mapControl.zapTask();
     }
-    else {
-        $('#G').prop('checked', true);
+
+    function showAltPreferences() {
+        if (prefs.altPrefs.altsource === 'P') {
+            $("#P").prop("checked", true);
+        }
+        else {
+            $('#G').prop('checked', true);
+        }
     }
- }
- 
- function  enterTask(points,zoomto) {
-     task.createTask(points);
-    var distance=task.getTaskLength();
-  var i;
-    var pointlabel;
-    $('#taskbuttons').html("");
+
+    function enterTask(points, zoomto) {
+        task.createTask(points);
+        var distance = task.getTaskLength();
+        var i;
+        var pointlabel;
+        $('#taskbuttons').html("");
         $('#taskinfo').html("");
-    for (i = 0; i < task.labels.length; i++) {
-      $('#taskinfo').append('<tr><th>' + task.labels[i] + ':</th><td>' + task.names[i] + ':</td><td>' + utils.showFormat(task.coords[i]) + '</td></tr>');
-      switch (i) {
-      case 0:
-        pointlabel = "Start";
-        break;
-      case task.labels.length - 1:
-        pointlabel = "Finish";
-        break;
-      default:
-        pointlabel = "TP" + i.toString();
-      }
-      $('#taskbuttons').append('&nbsp;<button>' + pointlabel + '</button>');
-    }
-    $('#tasklength').text("Task distance: " + prefs.showDistance(distance));
-     $('#task').show();
-     mapControl.addTask(points,zoomto);
-     $('#taskbuttons button').on('click', function (event) {
-        var li = $(this).index();
-        mapControl.showTP(task.coords[li]);
-      });
-}
- 
-function displayHeaders(headerList) {
- var headerBlock = $('#headers'); 
-    var headerIndex;
-      headerBlock.html('');
-    for (headerIndex = 0; headerIndex < headerList.length; headerIndex++) {
-        headerBlock.append('<tr><th>' + headerList[headerIndex].name + ":  </th><td>" + headerList[headerIndex].value + "</td></tr>");
-    }
-}
-
-function getFromPlanner(source) {
-    var planUrl = "../TaskMap/xcplan.php?version="  + source;
-    if ((!(planWindow)) || (planWindow.closed)) {
-      planWindow = window.open(planUrl, "_blank");
-    }
-    planWindow.focus();
-  }
-
-module.exports={
-    
-setSectors: function () {
-    var sectors= {
-    startrad : $('#startrad').val(),
-    finrad: $('#finishrad').val(),
-    tprad:  $('#tpbarrelrad').val(),
-    sector_rad: $('#tpsectorrad').val(),
-    sector_angle:  $('#subtends').val(),
-   use_sector:  $('#tpsector').prop('checked'),
-    use_barrel:  $('#tpbarrel').prop('checked'),
-    finishtype:  $("input[name=finishtype]:checked").val()
-};
-
-var saveit=$('#savesectors').prop('checked');
-if(prefs.setSectors(sectors,saveit)) {
-    mapControl.addSectors();
-    $('#sectordefs').hide();
-}
-},
-    
-showImported: function(points) {
-    enterTask(points,true);
-},
-
-showSectorPreferences: function() {
- $('#startrad').val(prefs.sectors.startrad);
- $('#finishrad').val(prefs.sectors.finrad);
- $('#tpbarrelrad').val(prefs.sectors.tprad);
- $('#tpsectorrad').val(prefs.sectors.sector_rad);
-$('#subtends').val(prefs.sectors.sector_angle);
-   if(prefs.sectors.use_sector) {
-        $('#tpsector').prop('checked',true);
-   }
-    if(prefs.sectors.use_barrel) {
-        $('#tpbarrel').prop('checked',true);
-    }
-    if(prefs.sectors.finishtype==='line') {
-       $("input[name=finishtype][value='line']").prop("checked",true);
-    }
-    else {
-              $("input[name=finishtype][value='circle']").prop("checked",true);
-    }
-},
-
-showPreferences: function() {
-    $('#altitudeunits').val(prefs.units.altitude);
-    $('#lengthunits').val(prefs.units.distance);
-     $('#climbunits').val(prefs.units.climb);
-     $('#cruiseunits').val(prefs.units.cruise);
-     $('#taskunits').val(prefs.units.task);
-    $('#airclip').val(prefs.airclip);
-    this.showSectorPreferences();
-    showAltPreferences();
-},
-
-getUserTask: function() {
-    var input;
-    var pointdata;
-    var success = true;
-    var taskdata = {
-      coords: [],
-      names: []
-    };
-    $("#requestdata :input[type=text]").each(function () {
-        input = $(this).val().replace(/ /g, '');
-        if (input.length > 0) {
-          pointdata = utils.getPoint(input);
-          if (pointdata.message === "OK") {
-            taskdata.coords.push(pointdata.coords);
-            taskdata.names.push(pointdata.name);
-          } else {
-            success = false;
-            alert("\"" + $(this).val() + "\"" + " not recognised-" + " ignoring entry");
-          }
+        for (i = 0; i < task.labels.length; i++) {
+            $('#taskinfo').append('<tr><th>' + task.labels[i] + ':</th><td>' + task.names[i] + ':</td><td>' + utils.showFormat(task.coords[i]) + '</td></tr>');
+            switch (i) {
+                case 0:
+                    pointlabel = "Start";
+                    break;
+                case task.labels.length - 1:
+                    pointlabel = "Finish";
+                    break;
+                default:
+                    pointlabel = "TP" + i.toString();
+            }
+            $('#taskbuttons').append('&nbsp;<button>' + pointlabel + '</button>');
         }
-      });
-     if(success===true) {
-         if(taskdata.names.length > 1) {
-             enterTask(taskdata,true);
-         }
-         $('#taskentry').hide();
-     }
-  },
-
-replaceTask: function(source) {
-      var taskpoints=null;
-     $('#taskentry').hide();
-      $('#task').hide();
-      switch(source) {
-          case 'igc':
-              if(flight.latLong.length >0) {
-                 if(flight.taskpoints.names.length > 1) {
-                    taskpoints=flight.taskpoints;
-                      enterTask(taskpoints,false);
-                 }
-                 else {
-                     alert("No task found in IGC file");
-                 }
-              }
-              break;
-          case 'user':
-              $('#taskentry').show();
-              break;
-          case 'world':
-              getFromPlanner('world');
-          case 'uk':
-              getFromPlanner('uk');
-              break;
-          case 'nix': 
-              zapTask();
-              break;
-      }
-},
-
-showPosition:  function(index) {
-    var altInfo=prefs.showAltitude(flight.pressureAltitude[index],flight.gpsAltitude[index],flight.takeOff.pressure,flight.takeOff.gps,flight.baseElevation);
-     var displaySentence=utils.unixToString((flight.recordTime[index] + flight.timeZone.offset + 86400)%86400) + ' ' + flight.timeZone. zoneAbbr + ': ';
-    displaySentence+= altInfo.displaySentence;
-    displaySentence+= ": " + utils.showFormat(flight.latLong[index]);
-    $('#timePositionDisplay').html(displaySentence);
-      var xval = 1000 * (flight.recordTime[index] + flight.timeZone.offset);
-      var yval = altInfo.altPos;
-      mapControl.setTimeMarker(flight.latLong[index]);
-       barogram.lockCrosshair({
-      x: xval,
-      y: yval
-    });
-},
-
-altChange: function(index) {
-    if(flight.latLong.length > 0) {
-        barogram.plot();
-        this.showPosition(index);
-    }
-},
-
-airClipChange: function() {
-    mapControl.showAirspace();
-},
-
-lengthChange: function() {
-    var distance=task.getTaskLength();
-      $('#tasklength').text("Task distance: " + prefs.showDistance(distance));
-},
-
-displayIgc: function(){
-var task=require('./task.js');
-displayHeaders(flight.headers);
-$('#timeSlider').val(0);
-$('#timeSlider').prop('max', flight.recordTime.length-1);
-mapControl.setBounds(flight.bounds);
-$.when(utils.getAirspace(flight.bounds,20)).done(function(args) {
-             mapControl.setAirspace(args);
-             mapControl.showAirspace();
-           });
-var _this=this;
- $.when(utils.getTimeZone(flight.unixStart[0],flight.latLong[0]),utils.getElevation(flight.latLong[0])).done(function (tzargs,elargs) {
-          if (tzargs[0].status === 'OK') {
-                  flight.timeZone.zoneAbbr = tzargs[0].timeZoneName.match(/[A-Z]/g).join('');
-                  flight.timeZone.offset = parseFloat(tzargs[0].rawOffset) + parseFloat(tzargs[0].dstOffset);
-                 flight.timeZone.zoneName = tzargs[0].timeZoneName;
-                  $('#datecell').text(utils.showDate(flight.unixStart[0] + flight.timeZone.offset));
-        } 
-        if (elargs[0].status === 'OK') {
-            flight.setBaseElevation(elargs[0].results[0].elevation);
-        }
-          barogram.plot();
-        _this.showPosition(0);
+        $('#tasklength').text("Task distance: " + prefs.showDistance(distance));
+        $('#task').show();
+        mapControl.addTask(points, zoomto);
+        $('#taskbuttons button').on('click', function (event) {
+            var li = $(this).index();
+            mapControl.showTP(task.coords[li]);
         });
-
-mapControl.addTrack(flight.latLong);
-if(prefs.tasksource==='igc') {
-    if(flight.taskpoints.names.length > 1) {
-    enterTask(flight.taskpoints,false);
-     }
-     else {
-         zapTask();
-     }
     }
-    $('#timeSlider').val(0);
-    $('#timeSlider').prop('max', flight.recordTime.length - 1);
 
-},
-
-zoomTrack: function() {
-    if(flight) {
-       mapControl.setBounds(flight.bounds);
+    function displayHeaders(headerList) {
+        var headerBlock = $('#headers');
+        var headerIndex;
+        headerBlock.html('');
+        for (headerIndex = 0; headerIndex < headerList.length; headerIndex++) {
+            headerBlock.append('<tr><th>' + headerList[headerIndex].name + ":  </th><td>" + headerList[headerIndex].value + "</td></tr>");
+        }
     }
-}
 
-}
+    function getFromPlanner(source) {
+        var planUrl = "../TaskMap/xcplan.php?version=" + source;
+        if ((!(planWindow)) || (planWindow.closed)) {
+            planWindow = window.open(planUrl, "_blank");
+        }
+        planWindow.focus();
+    }
+
+    module.exports = {
+
+        setSectors: function () {
+            var sectors = {
+                startrad: $('#startrad').val(),
+                finrad: $('#finishrad').val(),
+                tprad: $('#tpbarrelrad').val(),
+                sector_rad: $('#tpsectorrad').val(),
+                sector_angle: $('#subtends').val(),
+                use_sector: $('#tpsector').prop('checked'),
+                use_barrel: $('#tpbarrel').prop('checked'),
+                finishtype: $("input[name=finishtype]:checked").val()
+            };
+
+            var saveit = $('#savesectors').prop('checked');
+            if (prefs.setSectors(sectors, saveit)) {
+                mapControl.addSectors();
+                $('#sectordefs').hide();
+            }
+        },
+
+        showImported: function (points) {
+            enterTask(points, true);
+        },
+
+        showSectorPreferences: function () {
+            $('#startrad').val(prefs.sectors.startrad);
+            $('#finishrad').val(prefs.sectors.finrad);
+            $('#tpbarrelrad').val(prefs.sectors.tprad);
+            $('#tpsectorrad').val(prefs.sectors.sector_rad);
+            $('#subtends').val(prefs.sectors.sector_angle);
+            if (prefs.sectors.use_sector) {
+                $('#tpsector').prop('checked', true);
+            }
+            if (prefs.sectors.use_barrel) {
+                $('#tpbarrel').prop('checked', true);
+            }
+            if (prefs.sectors.finishtype === 'line') {
+                $("input[name=finishtype][value='line']").prop("checked", true);
+            }
+            else {
+                $("input[name=finishtype][value='circle']").prop("checked", true);
+            }
+        },
+
+        showPreferences: function () {
+            $('#altitudeunits').val(prefs.units.altitude);
+            $('#lengthunits').val(prefs.units.distance);
+            $('#climbunits').val(prefs.units.climb);
+            $('#cruiseunits').val(prefs.units.cruise);
+            $('#taskunits').val(prefs.units.task);
+            $('#airclip').val(prefs.airclip);
+            this.showSectorPreferences();
+            showAltPreferences();
+        },
+
+        getUserTask: function () {
+            var input;
+            var pointdata;
+            var success = true;
+            var taskdata = {
+                coords: [],
+                names: []
+            };
+            $("#requestdata :input[type=text]").each(function () {
+                input = $(this).val().replace(/ /g, '');
+                if (input.length > 0) {
+                    pointdata = utils.getPoint(input);
+                    if (pointdata.message === "OK") {
+                        taskdata.coords.push(pointdata.coords);
+                        taskdata.names.push(pointdata.name);
+                    } else {
+                        success = false;
+                        alert("\"" + $(this).val() + "\"" + " not recognised-" + " ignoring entry");
+                    }
+                }
+            });
+            if (success === true) {
+                if (taskdata.names.length > 1) {
+                    enterTask(taskdata, true);
+                }
+                $('#taskentry').hide();
+            }
+        },
+
+        replaceTask: function (source) {
+            var taskpoints = null;
+            $('#taskentry').hide();
+            $('#task').hide();
+            switch (source) {
+                case 'igc':
+                    if (flight.latLong.length > 0) {
+                        if (flight.taskpoints.names.length > 1) {
+                            taskpoints = flight.taskpoints;
+                            enterTask(taskpoints, false);
+                        }
+                        else {
+                            alert("No task found in IGC file");
+                        }
+                    }
+                    break;
+                case 'user':
+                    $('#taskentry').show();
+                    break;
+                case 'world':
+                    getFromPlanner('world');
+                    break;
+                case 'uk':
+                    getFromPlanner('uk');
+                    break;
+                case 'nix':
+                    zapTask();
+                    break;
+            }
+        },
+
+        showPosition: function (index) {
+            var altInfo = prefs.showAltitude(flight.pressureAltitude[index], flight.gpsAltitude[index], flight.takeOff.pressure, flight.takeOff.gps, flight.baseElevation);
+            var displaySentence = utils.unixToString((flight.recordTime[index] + flight.timeZone.offset + 86400) % 86400) + ' ' + flight.timeZone.zoneAbbr + ': ';
+            displaySentence += altInfo.displaySentence;
+            displaySentence += ": " + utils.showFormat(flight.latLong[index]);
+            $('#timePositionDisplay').html(displaySentence);
+            var xval = 1000 * (flight.recordTime[index] + flight.timeZone.offset);
+            var yval = altInfo.altPos;
+            mapControl.setTimeMarker(flight.latLong[index]);
+            barogram.lockCrosshair({
+                x: xval,
+                y: yval
+            });
+        },
+
+        altChange: function (index) {
+            if (flight.latLong.length > 0) {
+                barogram.plot();
+                this.showPosition(index);
+            }
+        },
+
+        airClipChange: function () {
+            mapControl.showAirspace();
+        },
+
+        lengthChange: function () {
+            var distance = task.getTaskLength();
+            $('#tasklength').text("Task distance: " + prefs.showDistance(distance));
+        },
+
+        displayIgc: function () {
+            var task = require('./task.js');
+            displayHeaders(flight.headers);
+            $('#timeSlider').val(0);
+            $('#timeSlider').prop('max', flight.recordTime.length - 1);
+            mapControl.setBounds(flight.bounds);
+            $.when(utils.getAirspace(flight.bounds, 20)).done(function (args) {
+                mapControl.setAirspace(args);
+                mapControl.showAirspace();
+            });
+            var _this = this;
+            $.when(utils.getTimeZone(flight.unixStart[0], flight.latLong[0]), utils.getElevation(flight.latLong[0])).done(function (tzargs, elargs) {
+                if (tzargs[0].status === 'OK') {
+                    flight.timeZone.zoneAbbr = tzargs[0].timeZoneName.match(/[A-Z]/g).join('');
+                    flight.timeZone.offset = parseFloat(tzargs[0].rawOffset) + parseFloat(tzargs[0].dstOffset);
+                    flight.timeZone.zoneName = tzargs[0].timeZoneName;
+                    $('#datecell').text(utils.showDate(flight.unixStart[0] + flight.timeZone.offset));
+                }
+                if (elargs[0].status === 'OK') {
+                    flight.setBaseElevation(elargs[0].results[0].elevation);
+                }
+                barogram.plot();
+                _this.showPosition(0);
+            });
+
+            mapControl.addTrack(flight.latLong);
+            if (prefs.tasksource === 'igc') {
+                if (flight.taskpoints.names.length > 1) {
+                    enterTask(flight.taskpoints, false);
+                }
+                else {
+                    zapTask();
+                }
+            }
+            $('#timeSlider').val(0);
+            $('#timeSlider').prop('max', flight.recordTime.length - 1);
+
+        },
+
+        zoomTrack: function () {
+            if (flight) {
+                mapControl.setBounds(flight.bounds);
+            }
+        }
+
+    };
+})();
